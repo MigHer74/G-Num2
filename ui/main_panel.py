6661936acbcaf41fe893b08a02d191091f832992
Miguel Hernandez <migher74@outlook.com>
2025-07-23 19:55:52 -0600

    Add labels for short and long game results to the main panel.

--- ui/main_panel.py
+++ ui/main_panel.py
@@ -1,4 +1,4 @@
-from ttkbootstrap import Window, Labelframe, Radiobutton, Entry
+from ttkbootstrap import Window, Labelframe, Radiobutton, Entry, Label
 from ttkbootstrap import StringVar
 
 
@@ -54,29 +54,32 @@ class MainPanel(Window):
         self.fr_short_game.grid(row=0, column=1, padx=(15, 15), pady=(15, 15),
                                 sticky="n")
 
+        self.label_short_01 = Label(self.fr_short_game, text="Game #1")
+        self.label_short_01.grid(row=0, column=0, padx=(15, 0), pady=(15, 15))
+
         self.entry_item_01 = Entry(self.fr_short_game, width=3,
                                    justify="center")
-        self.entry_item_01.grid(row=1, column=0, padx=(15, 0), pady=(15, 15))
+        self.entry_item_01.grid(row=0, column=1, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_02 = Entry(self.fr_short_game, width=3,
                                    justify="center")
-        self.entry_item_02.grid(row=1, column=1, padx=(15, 0), pady=(15, 15))
+        self.entry_item_02.grid(row=0, column=2, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_03 = Entry(self.fr_short_game, width=3,
                                    justify="center")
-        self.entry_item_03.grid(row=1, column=2, padx=(15, 0), pady=(15, 15))
+        self.entry_item_03.grid(row=0, column=3, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_04 = Entry(self.fr_short_game, width=3,
                                    justify="center")
-        self.entry_item_04.grid(row=1, column=3, padx=(15, 0), pady=(15, 15))
+        self.entry_item_04.grid(row=0, column=4, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_05 = Entry(self.fr_short_game, width=3,
                                    justify="center")
-        self.entry_item_05.grid(row=1, column=4, padx=(15, 0), pady=(15, 15))
+        self.entry_item_05.grid(row=0, column=5, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_06 = Entry(self.fr_short_game, width=3,
                                    justify="center")
-        self.entry_item_06.grid(row=1, column=5, padx=(15, 15), pady=(15, 15))
+        self.entry_item_06.grid(row=0, column=6, padx=(15, 15), pady=(15, 15))
 
     def panel_long_game(self):
         self.fr_long_game = Labelframe(self,
@@ -85,74 +88,83 @@ class MainPanel(Window):
         self.fr_long_game.grid(row=1, column=1, padx=(15, 15), pady=(15, 15),
                                sticky="n")
 
+        self.label_long_01 = Label(self.fr_long_game, text="Game #1")
+        self.label_long_01.grid(row=0, column=0, padx=(15, 0), pady=(15, 15))
+
         self.entry_item_11 = Entry(self.fr_long_game, width=3,
                                    justify="center")
-        self.entry_item_11.grid(row=1, column=0, padx=(15, 0), pady=(15, 15))
+        self.entry_item_11.grid(row=0, column=1, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_12 = Entry(self.fr_long_game, width=3,
                                    justify="center")
-        self.entry_item_12.grid(row=1, column=1, padx=(15, 0), pady=(15, 15))
+        self.entry_item_12.grid(row=0, column=2, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_13 = Entry(self.fr_long_game, width=3,
                                    justify="center")
-        self.entry_item_13.grid(row=1, column=2, padx=(15, 0), pady=(15, 15))
+        self.entry_item_13.grid(row=0, column=3, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_14 = Entry(self.fr_long_game, width=3,
                                    justify="center")
-        self.entry_item_14.grid(row=1, column=3, padx=(15, 0), pady=(15, 15))
+        self.entry_item_14.grid(row=0, column=4, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_15 = Entry(self.fr_long_game, width=3,
                                    justify="center")
-        self.entry_item_15.grid(row=1, column=4, padx=(15, 0), pady=(15, 15))
+        self.entry_item_15.grid(row=0, column=5, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_16 = Entry(self.fr_long_game, width=3,
                                    justify="center")
-        self.entry_item_16.grid(row=1, column=5, padx=(15, 15), pady=(15, 15))
+        self.entry_item_16.grid(row=0, column=6, padx=(15, 15), pady=(15, 15))
+
+        self.label_long_02 = Label(self.fr_long_game, text="Game #2")
+        self.label_long_02.grid(row=1, column=0, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_21 = Entry(self.fr_long_game, width=3,
                                    justify="center")
-        self.entry_item_21.grid(row=2, column=0, padx=(15, 0), pady=(15, 15))
+        self.entry_item_21.grid(row=1, column=1, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_22 = Entry(self.fr_long_game, width=3,
                                    justify="center")
-        self.entry_item_22.grid(row=2, column=1, padx=(15, 0), pady=(15, 15))
+        self.entry_item_22.grid(row=1, column=2, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_23 = Entry(self.fr_long_game, width=3,
                                    justify="center")
-        self.entry_item_23.grid(row=2, column=2, padx=(15, 0), pady=(15, 15))
+        self.entry_item_23.grid(row=1, column=3, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_24 = Entry(self.fr_long_game, width=3,
                                    justify="center")
-        self.entry_item_24.grid(row=2, column=3, padx=(15, 0), pady=(15, 15))
+        self.entry_item_24.grid(row=1, column=4, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_25 = Entry(self.fr_long_game, width=3,
                                    justify="center")
-        self.entry_item_25.grid(row=2, column=4, padx=(15, 0), pady=(15, 15))
+        self.entry_item_25.grid(row=1, column=5, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_26 = Entry(self.fr_long_game, width=3,
                                    justify="center")
-        self.entry_item_26.grid(row=2, column=5, padx=(15, 15), pady=(15, 15))
+        self.entry_item_26.grid(row=1, column=6, padx=(15, 15), pady=(15, 15))
+
+        self.label_long_03 = Label(self.fr_long_game, text="Game #3")
+        self.label_long_03.grid(row=2, column=0, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_31 = Entry(self.fr_long_game, width=3,
                                    justify="center")
-        self.entry_item_31.grid(row=3, column=0, padx=(15, 0), pady=(15, 15))
+        self.entry_item_31.grid(row=2, column=1, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_32 = Entry(self.fr_long_game, width=3,
                                    justify="center")
-        self.entry_item_32.grid(row=3, column=1, padx=(15, 0), pady=(15, 15))
+        self.entry_item_32.grid(row=2, column=2, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_33 = Entry(self.fr_long_game, width=3,
                                    justify="center")
-        self.entry_item_33.grid(row=3, column=2, padx=(15, 0), pady=(15, 15))
+        self.entry_item_33.grid(row=2, column=3, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_34 = Entry(self.fr_long_game, width=3,
                                    justify="center")
-        self.entry_item_34.grid(row=3, column=3, padx=(15, 0), pady=(15, 15))
+        self.entry_item_34.grid(row=2, column=4, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_35 = Entry(self.fr_long_game, width=3,
                                    justify="center")
-        self.entry_item_35.grid(row=3, column=4, padx=(15, 0), pady=(15, 15))
+        self.entry_item_35.grid(row=2, column=5, padx=(15, 0), pady=(15, 15))
 
         self.entry_item_36 = Entry(self.fr_long_game, width=3,
                                    justify="center")
-        self.entry_item_36.grid(row=3, column=5, padx=(15, 15), pady=(15, 15))
+        self.entry_item_36.grid(row=2, column=6, padx=(15, 15), pady=(15, 15))
