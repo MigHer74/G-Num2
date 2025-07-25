a6d0a7b6aa4766cf71f4696fbc123da97d0b6d70
Miguel Hernandez <migher74@outlook.com>
2025-07-24 20:32:24 -0600

    Add function to create 'games' table in the database.

--- src/db_tools.py
+++ src/db_tools.py
@@ -4,3 +4,24 @@ import sqlite3
 def connect_db():
     db_connection = sqlite3.connect("db/gnum2.db")
     return db_connection
+
+
+def create_table():
+    dbcon = connect_db()
+    dbcur = dbcon.cursor()
+
+    sqlrow = """CREATE TABLE IF NOT EXISTS 'games' (
+    game_type TEXT,
+    game_week TEXT,
+    game_number INTEGER,
+    number_1 INTEGER,
+    number_2 INTEGER,
+    number_3 INTEGER,
+    number_4 INTEGER,
+    number_5 INTEGER,
+    number_6 INTEGER
+    );"""
+
+    dbcur.execute(sqlrow)
+    dbcon.commit()
+    dbcon.close()
