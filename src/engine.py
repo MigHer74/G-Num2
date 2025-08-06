15a3a17f5a98d65f0439c46896874fe0ba41b2c9
Miguel Hernandez <migher74@outlook.com>
2025-08-05 20:40:02 -0600

    Separate the different values according to the number of repetitions and the type of game.

--- src/engine.py
+++ src/engine.py
@@ -1,7 +1,7 @@
 import src.db_tools as db
 
 
-def game_engine(game_type):
+def game_engine(game_type: str) -> list:
     all_values = []
     row_results_1 = []
     row_results_2 = []
@@ -16,3 +16,34 @@ def game_engine(game_type):
             all_values.append(number)
 
     all_values.sort()
+
+    if game_type == "short":
+        for number in range(1, 40):
+            choice = all_values.count(number)
+
+            if choice >= 5:
+                row_results_1.append(str(number))
+            elif choice == 4:
+                row_results_2.append(str(number))
+            elif choice == 3:
+                row_results_3.append(str(number))
+            elif choice == 2:
+                row_results_4.append(str(number))
+
+            choice = 0
+    else:
+        for number in range(1, 57):
+            choice = all_values.count(number)
+
+            if choice >= 6:
+                row_results_1.append(str(number))
+            elif choice == 5:
+                row_results_2.append(str(number))
+            elif choice == 4:
+                row_results_3.append(str(number))
+            elif choice == 3:
+                row_results_4.append(str(number))
+
+            choice = 0
+
+        return row_results_1, row_results_2, row_results_3, row_results_4
