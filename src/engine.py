d0f4a8f397560a8deaa4a7abf5f6967c40e009a5
Miguel Hernandez <migher74@outlook.com>
2025-08-08 20:00:04 -0600

    create final_values function to obtain the 6 numbers with the most repetitions, if the 6 numbers are not obtained the function will deliver the missing numbers randomly from the following repetitions.

--- src/engine.py
+++ src/engine.py
@@ -1,3 +1,4 @@
+import random as rdm
 import src.db_tools as db
 
 
@@ -46,4 +47,30 @@ def game_engine(game_type: str) -> list:
 
             choice = 0
 
-        return row_results_1, row_results_2, row_results_3, row_results_4
+    row_final = final_values(row_results_1, row_results_2, row_results_3,
+                             row_results_4)
+
+    return row_results_1, row_results_2, row_results_3, row_results_4, \
+        row_final
+
+
+def final_values(row1: list, row2: list, row3: list, row4: list) -> list:
+    final_list = []
+    temp_list = []
+
+    for row in [row1, row2, row3, row4]:
+        if len(final_list) < 6:
+            if len(row) != 0 and len(row) <= 6:
+                for number in row:
+                    if len(final_list) < 6:
+                        final_list.append(number)
+                    else:
+                        break
+            elif len(row) > 6:
+                temp_list = row.copy()
+                for item in range(6 - len(final_list)):
+                    selection = rdm.choice(temp_list)
+                    final_list.append(selection)
+                    temp_list.remove(selection)
+
+    return final_list
